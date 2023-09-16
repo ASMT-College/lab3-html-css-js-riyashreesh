95d5d458b83a8008137d696542635c070a6dc85e
Reya shreesh <reeyashreesh82@gmail.com>
2023-09-16 18:43:36 +0545

    lab3

---
+++ clock.js
@@ -0,0 +1,20 @@
+function showDateAndTime() {
+  document.getElementById("display_date_and_time").innerHTML = Date();
+}
+//setTimeout(showDate, 5000);
+// setInterval(showDateAndTime, 1000);
+var i = setInterval(showDateAndTime, 1000);
+function clearDateMove() {
+  clearInterval(i);
+}
+setTimeout(clearDateMove, 5);
+
+function showClock() {
+  var todayDate = new Date();
+  var hour = todayDate.getHours();
+  var minute = todayDate.getMinutes();
+  var second = todayDate.getSeconds();
+  document.getElementById("display_clock").innerHTML =
+    hour + ":" + minute + ":" + second;
+}
+setInterval(showClock, 1000);
